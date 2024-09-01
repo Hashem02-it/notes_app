98298386091063fd364f9546f20e15c7d3c4ffa8
Hashem02-it <hashemabuain02it@gmail.com>
2024-09-01 23:30:55 +0300

    create edit notes view

--- lib/views/widgets/cutom_note_item.dart
+++ lib/views/widgets/cutom_note_item.dart
@@ -1,49 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 29, left: 16, top: 29),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Color(0xffFFCC80),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutetr tips',
-              style: TextStyle(fontSize: 28, color: Colors.black),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) {
+            return EditNoteView();
+          }),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(bottom: 29, left: 16, top: 29),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Color(0xffFFCC80),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutetr tips',
+                style: TextStyle(fontSize: 28, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 15),
+                child: Text(
+                  'build ypur career with hashem',
+                  style: TextStyle(
+                      fontSize: 18, color: Colors.black.withOpacity(0.5)),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    size: 30,
+                    Icons.delete,
+                    color: Colors.black,
+                  )),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 15),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'build ypur career with hashem',
+                'may21, 2022',
                 style: TextStyle(
-                    fontSize: 18, color: Colors.black.withOpacity(0.5)),
+                    fontSize: 12, color: Colors.black.withOpacity(0.5)),
               ),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  size: 30,
-                  Icons.delete,
-                  color: Colors.black,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'may21, 2022',
-              style:
-                  TextStyle(fontSize: 12, color: Colors.black.withOpacity(0.5)),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
