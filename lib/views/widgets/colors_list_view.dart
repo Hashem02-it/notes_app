0d74e06bb5d70525663e3e9f46bc478028494a23
Hashem02-it <hashemabuain02it@gmail.com>
2024-09-06 21:09:28 +0300

    colors list view pt2

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isPicked, required this.color});
 
+  final bool isPicked;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 32,
-      backgroundColor: Colors.amber,
-    );
+    return isPicked
+        ? CircleAvatar(
+            radius: 32,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 30,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 32,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = [
+    Color(0xffD8F793),
+    Color(0xffA0CA92),
+    Color(0xff75B09C),
+    Color(0xff998650),
+    Color(0xffE0BE36),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 32 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return ColorItem();
+          return Padding(
+            padding: const EdgeInsets.only(left: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isPicked: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
